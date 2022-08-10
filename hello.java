e38f389169ec1da803b76afe17a173cc307a6e33
ALAIN GIRES EBOUEL NZOM <90434281+AlainGiresEbouelNzom@users.noreply.github.com>
2022-08-09 22:14:14 -0400

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,6 +1,6 @@
 public class Hello {
         public static void main(String[ ] args) {
-                System.out.println("Hello World gigi!");
+                System.out.println("Hello World gires!");
                  System.out.println("Hello everyone");
         }
 }
